0bc411538442b1d6fbeea46e6daa80e86e11aff3
BullatBor <98647011+BullatBor@users.noreply.github.com>
2023-03-06 22:44:00 +0700

    home

---
+++ server/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();//счтывание файла
+const express = require('express'); 
+const sequelize = require('./db')//экспорт файла db
+const models = require('./models/models')//экспорт "таблиц"
+const cors = require('cors');//для отправления запроса с браузера устн в начале
+const router = require('./routes/index');
+
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api', router); // "api/ПУТЬ из routes"
+
+
+const start = async () => {
+    try{
+        await sequelize.authenticate();//подключение к базе данных
+        await sequelize.sync(); //сверять состояние БД со схемой данных
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    } catch(e){
+  console.log(e);
+    }
+}
+
+
+start();
